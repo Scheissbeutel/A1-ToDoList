26d7edf19a87c6fcc089ddd5ca106500a83e6679
Scheissbeutel <brickskill3r@gmail.com>
2025-03-21 23:04:03 -0300

    Commit de modificaciones en el css para el diseño de input y fecha

--- script.js
+++ script.js
@@ -10,3 +10,33 @@ document.getElementById("addTask").addEventListener("click", function() {
         taskInput.value = "";
     }
 });
+
+
+
+
+
+function actualizarHora(){
+    const opciones = { timeZone: 'America/Argentina/Buenos_Aires', hour12: false};
+    const fecha = new Date().toLocaleTimeString('es-AR', opciones);
+    document.getElementById('hora').textContent = fecha;
+}
+
+//Metodo para actualizar la hora cada segundo
+
+setInterval(actualizarHora, 1000);
+
+//Llamamos a la funcion una vez para que la hora aparezca de una
+
+actualizarHora();
+
+
+function actualizarFecha() {
+    const opciones = { timeZone: 'America/Argentina/Buenos_Aires', year: 'numeric', month: '2-digit', day: '2-digit' };
+    const fecha = new Date().toLocaleDateString('es-AR', opciones);
+    document.getElementById('fechaActual').textContent = fecha;
+}
+
+// Llamar a la función una vez para que la fecha aparezca inmediatamente
+actualizarFecha();
+
+
